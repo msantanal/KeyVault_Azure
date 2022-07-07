d3b6c7e3bea27e1c7e98f3f670468e756ee983fc
msantanal <108295791+msantanal@users.noreply.github.com>
2022-07-07 13:29:26 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
-data "azurerm_resource_group" "rg_oracle" {
+data "azurerm_resource_group" "RG" {
   name = var.resource_group_name
 }
 
 data azurerm_client_config current {}
 
-resource"azurerm_key_vault" "keyvault_oracle" {
+resource"azurerm_key_vault" "keyvault" {
   name                = var.name_keyvault
   location            = var.location
-  resource_group_name = data.azurerm_resource_group.rg_oracle.name
-  tenant_id           = var.tenant_id
+  resource_group_name = data.azurerm_resource_group.RG.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = var.sku_keyvault
   #add permission
   access_policy {
@@ -27,22 +27,4 @@ resource"azurerm_key_vault" "keyvault_oracle" {
   tags = {
     Environment = "Deployment"
   }
-}
-
-# Generate Ramdon String
-resource "random_string" "random_passwd" {
-  length                       = "14"
-  special                      = "true"
-}
-# Create the secret for user
-resource "azurerm_key_vault_secret" "user_secret" {
-  name                         = var.usersecret_name
-  value                        = random_string.random_passwd.result
-  key_vault_id                 = azurerm_key_vault.rg_oracle.id
-}
-# Create the secret for password
-resource "azurerm_key_vault_secret" "pass_secret" {
-  name                         = var.passwordsecret_name
-  value                        = random_string.random_passwd.result
-  key_vault_id                 = azurerm_key_vault.rg_oracle.id
 }
