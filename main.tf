91e61bf9caef4c7b7079bda521952a38de88d6c4
msantanal <108295791+msantanal@users.noreply.github.com>
2022-07-07 14:06:52 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,13 +15,9 @@ resource"azurerm_key_vault" "keyvault" {
     tenant_id         = data.azurerm_client_config.current.tenant_id
     object_id         = data.azurerm_client_config.current.object_id
   
-  secret_permissions = {
-    "get",
-    "delete",
-    "list",
-    "recover",
-    "set"
-  }
+  secret_permissions = [
+      "Get",
+    ]
 }
 }
   tags = {
