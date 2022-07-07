049391d1cdb7d7ff48c21a3d7ec9a5e609ad308b
msantanal <108295791+msantanal@users.noreply.github.com>
2022-07-07 13:26:15 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,8 +12,8 @@ resource"azurerm_key_vault" "keyvault_oracle" {
   sku_name            = var.sku_keyvault
   #add permission
   access_policy {
-    tenant_id         = data.azurerm_config.current.tenant_id
-    object_id         = data.azurerm.config.curent.object_id
+    tenant_id         = data.azurerm_client_config.current.tenant_id
+    object_id         = data.azurerm_client_config.current.object_id
   
   secret_permissions = {
     "get"
